d42ff3488a92fc3b09ec340f3926e0a076f65f91
Connor Giles <connor@connorgiles.com>
2020-06-20 14:30:26 -0400

    Added filter preview

--- src/components/TodoFilters.js
+++ src/components/TodoFilters.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse, Button } from 'reactstrap';
 
 import { statuses } from '../constants';
 
 import Badge from './Badge';
 
+const FiltersPreview = ({ filters }) => {
+  if (!filters || Object.keys(filters).length === 0) return <></>;
+  return (
+    <div className="d-inline-block ml-3 vertical-align-center">
+      {filters.status && (
+        <span>
+          Status: <Badge className="mr-2" status={filters.status} />
+        </span>
+      )}
+      {filters.tag && (
+        <span>
+          Tag: <Badge className="mr-2" status={filters.tag} />
+        </span>
+      )}
+    </div>
+  );
+};
+
 export default function TodoFilters({ filters, setFilters, tags }) {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const toggle = () => setShowFilters(!showFilters);
+
   const filterStatus = (status) =>
     setFilters({
       ...filters,
@@ -21,43 +44,57 @@ export default function TodoFilters({ filters, setFilters, tags }) {
   const isSelected = (type, val) => !filters[type] || filters[type] === val;
 
   return (
-    <div
-      className="py-3 fixed-bottom bg-secondary text-white"
-      style={{ backgroundColor: '#dae4ed' }}>
-      <div className="my-3 text-center">
-        Status:{' '}
-        <Badge
-          className="mr-2"
-          color="light"
-          status={'All'}
-          onClick={() => filterStatus(undefined)}
-        />
-        {statuses.map((s) => (
+    <div className="fixed-bottom">
+      <div style={{ marginBottom: '1rem', marginLeft: '1rem' }}>
+        <Button
+          color="secondary"
+          onClick={toggle}
+          style={{
+            borderRadius: '100px',
+          }}>
+          {showFilters ? 'Hide' : 'Show'} Filters
+        </Button>{' '}
+        <FiltersPreview filters={filters} />
+      </div>
+      <Collapse
+        isOpen={showFilters}
+        className="py-3 bg-secondary text-white"
+        style={{ opacity: 0.9 }}>
+        <div className="my-3 text-center">
+          Status:{' '}
           <Badge
             className="mr-2"
-            status={s}
-            color={isSelected('status', s) ? undefined : 'light'}
-            onClick={() => filterStatus(s)}
+            color="light"
+            status={'All'}
+            onClick={() => filterStatus(undefined)}
           />
-        ))}
-      </div>
-      <div className="my-3 text-center">
-        Tags:{' '}
-        <Badge
-          className="mr-2"
-          color={!filters.tag ? 'dark' : 'light'}
-          status={'All'}
-          onClick={() => filterTags(undefined)}
-        />
-        {tags.map(({ value }) => (
+          {statuses.map((s) => (
+            <Badge
+              className="mr-2"
+              status={s}
+              color={isSelected('status', s) ? undefined : 'light'}
+              onClick={() => filterStatus(s)}
+            />
+          ))}
+        </div>
+        <div className="my-3 text-center">
+          Tags:{' '}
           <Badge
             className="mr-2"
-            status={value}
-            color={filters.tag && isSelected('tag', value) ? 'dark' : 'light'}
-            onClick={() => filterTags(value)}
+            color={!filters.tag ? 'dark' : 'light'}
+            status={'All'}
+            onClick={() => filterTags(undefined)}
           />
-        ))}
-      </div>
+          {tags.map(({ value }) => (
+            <Badge
+              className="mr-2"
+              status={value}
+              color={filters.tag && isSelected('tag', value) ? 'dark' : 'light'}
+              onClick={() => filterTags(value)}
+            />
+          ))}
+        </div>
+      </Collapse>
     </div>
   );
 }
